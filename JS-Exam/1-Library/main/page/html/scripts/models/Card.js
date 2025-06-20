7f75d3120aec4941738f96b79d6016c261bf9fa3
Rythe25 <kyseanghor232@gmail.com>
2025-06-20 09:48:54 +0700

    Delete JS-Exam/1-Library directory

--- JS-Exam/1-Library/main/page/html/scripts/models/Card.js
+++
@@ -1,9 +0,0 @@
-export class Card{
-    constructor(id, visitorId, bookId, borrowDate, returnDate = null){
-        this.id = id;
-        this.visitorId = visitorId;
-        this.bookId = bookId;
-        this.borrowDate = borrowDate;
-        this.returnDate = returnDate;
-    }
-}
--- JS-Exam/1-Library/main/page/html/scripts/models/Visitor.js
+++
@@ -1,7 +0,0 @@
-export class Visitor {
-    constructor(id, name, phone) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-    }
-}
